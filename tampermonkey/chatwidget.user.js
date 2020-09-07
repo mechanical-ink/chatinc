9b71ed1c92a0fddc7f1d220dcd08871f711fa315
Schalk Neethling <sneethling@mozilla.com>
2020-09-07 15:28:41 +0200

    user script to test chat widget on Fullhouse Furniture
    A Tamper Monkey user script that can be used to test the chat widget
    on the Fullhouse Furniture website.

---
+++ tampermonkey/chatwidget.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         ChatWidget
+// @namespace    https://www.experio.co.za/
+// @version      0.1
+// @description  Embed chat widget on Fullhouse Furniture Website
+// @author       Mechanical Ink
+// @match        https://fullhousefurniture.co.za
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const widgetURL = "https://mechanical-ink.github.io/chatinc-1/";
+    const iframe = document.createElement('iframe');
+    iframe.src = widgetURL;
+
+    iframe.style.width = "500px";
+    iframe.style.height = "600px";
+    iframe.style.position = "fixed";
+    iframe.style.bottom = "0";
+    iframe.style.right = "0";
+    iframe.style.border = "0";
+    // ensure the widget is above everything else
+    iframe.style.zIndex = "99999";
+
+    document.documentElement.appendChild(iframe);
+})();
